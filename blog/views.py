3caa4a062e09371871697715959dc04841f9e67d
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-20 22:52:12 +1100

    Handle DoesNotExist Exception

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from blog.models import Comment, Post, Tag
 
@@ -47,7 +47,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -89,7 +89,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5].annotate(posts_with_tag_count=Count('posts'))
 
